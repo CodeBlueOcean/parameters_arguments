102b7cd49fbe88025926ace943a7c8000bb3f298
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-05 00:55:39 -0600

    Add files via upload

---
+++ parameters-arguments.py
@@ -0,0 +1,11 @@
+#parameters
+def say_hello(name, emoji):
+    print(f'hellllooo {name} {emoji}')
+
+#arguments
+say_hello('Andrei', '😃') #call, invoke
+say_hello('Dan', '😃') #call, invoke
+say_hello('Dan', '😃') #call, invoke
+
+
+
